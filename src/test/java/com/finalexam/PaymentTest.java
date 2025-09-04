c914537178d3d53a8cb8ee00db02281f101b7fb3
sitraka <sitraka7011@gmail.com>
2025-09-04 12:12:58 +0300

    add: seconds to payment test

--- src/test/java/com/finalexam/PaymentTest.java
+++ src/test/java/com/finalexam/PaymentTest.java
@@ -10,13 +10,13 @@ public class PaymentTest {
         
         @Test
         void testDeadline() {
-                Payment payment = new Payment(1, PaymentType.MOBILE_MONEY, 216000.0, Instant.now().plusSeconds(300));
+                Payment payment = new Payment(1, PaymentType.MOBILE_MONEY, 216000.0, Instant.now().plusSeconds(3600));
                 payment.setDeadline(Instant.now());
                 assertEquals(Instant.now(), payment.getDeadline());
         }
         @Test
         void testSetAmount() {
-                Payment payment = new Payment(1, PaymentType.BANK_CARD, 350000.0, Instant.now().plusSeconds(1800));
+                Payment payment = new Payment(1, PaymentType.BANK_CARD, 350000.0, Instant.now().plusSeconds(14400));
                 payment.setAmount(350000.0);
                 assertEquals(350000.0, payment.getAmount());
         }
